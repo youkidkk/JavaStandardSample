ec6555ebb032009e22762f9725032bf373ab0536
youkidkk <youkidkk80@gmail.com>
2017-02-04 14:56:21 +0900

    サンプル更新

--- Java8Sample/src/main/java/java8/sample/MethodReferenceSample.java
+++ Java8Sample/src/main/java/java8/sample/MethodReferenceSample.java
@@ -3,6 +3,7 @@ package java8.sample;
 import com.google.common.collect.Lists;
 
 import java.util.List;
+import java.util.function.Supplier;
 
 /**
  * メソッド参照 サンプル
@@ -14,8 +15,14 @@ public class MethodReferenceSample {
         List<String> list = Lists.newArrayList("foo", "bar", "baz");
 
         // List#forEach の引数の型 Consumer は戻り値なしで引数１個
-        // System.out#println も同様のため、メソッド参照で代入できる
-        list.forEach(System.out::println);
+        // System.out#print も同様のため、メソッド参照で代入できる
+        list.forEach(System.out::print); // -> foobarbaz
+
+        System.out.println();
+
+        // List#size は戻り値 int で引数なしのため Supplier に代入できる
+        Supplier<Integer> supplier = list::size;
+        System.out.println(supplier.get()); // -> 3
     }
 
 }
