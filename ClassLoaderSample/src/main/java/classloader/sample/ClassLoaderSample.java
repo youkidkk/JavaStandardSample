a6d66be458f03ec2918c2289510ccf92624267d5
youkidkk <youkidkk80@gmail.com>
2017-02-19 10:29:28 +0900

    ClassLoaderSample を更新

--- ClassLoaderSample/src/main/java/classloader/sample/ClassLoaderSample.java
+++ ClassLoaderSample/src/main/java/classloader/sample/ClassLoaderSample.java
@@ -1,16 +1,44 @@
 package classloader.sample;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
+import java.util.function.Function;
 
 @SuppressWarnings("javadoc")
 public class ClassLoaderSample {
 
-    public static void main(String[] args) throws IOException {
-        Properties properties = new Properties();
-        properties.load(ClassLoader.getSystemResourceAsStream("sample.properties"));
+    public void run() throws IOException {
+        final String resourceName = "sample.properties";
 
-        System.out.println(properties.getProperty("sample"));
+        // this.getClass()#getResourceAsStream の場合は、
+        // 同一パッケージ階層からリソースを取得する
+        System.out.println(this.getProperty(resourceName,
+                this.getClass()::getResourceAsStream));
+
+        // ClassLoader.getSystemResourceAsStream の場合は、
+        // デフォルトパッケージからリソースを取得する
+        System.out.println(this.getProperty(resourceName,
+                ClassLoader::getSystemResourceAsStream));
+
+        // ClassLoader.getSystemResourceAsStream でパッケージ階層を指定した場合は、
+        // そのパッケージ階層からリソースを取得する
+        System.out.println(this.getProperty(
+                this.getClass().getPackage().getName().replace(".", "/") + "/" + resourceName,
+                ClassLoader::getSystemResourceAsStream));
+    }
+
+    protected String getProperty(String resourceName,
+            Function<String, InputStream> loadFunction) throws IOException {
+        try (InputStream inStream = loadFunction.apply(resourceName)) {
+            Properties properties = new Properties();
+            properties.load(inStream);
+            return properties.getProperty("sample");
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        new ClassLoaderSample().run();
     }
 
 }
